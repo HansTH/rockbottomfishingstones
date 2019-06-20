737c46d2a399673c21f7cc7f645d468c4c46646c
Hans ter Horst <hbhorst@mac.com>
2019-06-20 11:47:20 +0200

    refactor to function components

--- src/components/global/TextAreaField.js
+++ src/components/global/TextAreaField.js
@@ -2,52 +2,50 @@ import React, { Component } from 'react';
 import styled from 'styled-components';
 import { ThemeColors } from '../../styles/elements';
 
-export default class TextAreaField extends Component {
-  state = {
-    name: ''
-  };
+export default function TextAreaField(props) {
+	const {
+		label,
+		name,
+		cols,
+		rows,
+		placeholder,
+		isRequired,
+		onChange,
+		value
+	} = props;
 
-  handleChange = e => {
-    this.setState({
-      [e.target.name]: e.target.value
-    });
-  };
-
-  render() {
-    const { label, name, cols, rows, placeholder, isRequired } = this.props;
-    return (
-      <TextArea>
-        <label htmlFor='name'>{label}</label>
-        <textarea
-          name={name}
-          value={this.state.name}
-          cols={cols}
-          rows={rows}
-          placeholder={placeholder}
-          required={isRequired}
-          onChange={this.handleChange}
-        />
-      </TextArea>
-    );
-  }
+	return (
+		<TextArea>
+			<label htmlFor='name'>{label}</label>
+			<textarea
+				name={name}
+				value={value}
+				cols={cols}
+				rows={rows}
+				placeholder={placeholder}
+				required={isRequired}
+				onChange={onChange}
+			/>
+		</TextArea>
+	);
 }
 
 const TextArea = styled.div`
-  display: flex;
-  flex-direction: column;
-  margin: 1rem 0;
+	display: flex;
+	flex-direction: column;
+	margin: 1rem 0;
 
-  label {
-    color: ${ThemeColors.green};
-    font-size: 1.1rem;
-  }
+	label {
+		color: ${ThemeColors.green};
+		font-size: 1.1rem;
+	}
 
-  input,
-  textarea {
-    font-size: 1rem;
-    padding: 0.5rem;
-    border: none;
-    border-radius: 5px;
-    outline: none;
-  }
+	input,
+	textarea {
+		font-size: 1rem;
+		padding: 0.5rem;
+		border: none;
+		border-radius: 5px;
+		outline: none;
+	}
 `;
