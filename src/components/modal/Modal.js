3c7d742cb4eaa921b190be96ec5970add74b7f9e
Hans ter Horst <hbhorst@mac.com>
2019-06-24 20:53:53 +0200

    add message and info props, change CSS styling

--- src/components/modal/Modal.js
+++ src/components/modal/Modal.js
@@ -1,43 +1,45 @@
-import React, { Component } from 'react';
+import React from 'react';
 import styled from 'styled-components';
 import { ThemeColors } from '../../styles/elements';
 import { fixed } from '../../styles/utils';
 
-export class Modal extends Component {
-  render() {
-    return (
-      <ModalContainer isOpen={this.props.isModalOpen}>
-        <ModalHeader>
-          {this.props.response.status === 200 ? (
-            <h3>Email is verzonden</h3>
-          ) : (
-            <div>
-              <h3>Email niet verzonden</h3>
-              <p>{this.props.response.text}</p>
-            </div>
-          )}
-        </ModalHeader>
-      </ModalContainer>
-    );
-  }
+export default function Modal(props) {
+	const { isModalOpen, response, message, info } = props;
+	return (
+		<ModalContainer isOpen={isModalOpen}>
+			<ModalHeader>
+				{response.status === 200 ? (
+					<>
+						<h3>{message}</h3>
+						<p>{info}</p>
+					</>
+				) : (
+					<>
+						<h3>Email niet verzonden</h3>
+						<p>{response.text}</p>
+					</>
+				)}
+			</ModalHeader>
+		</ModalContainer>
+	);
 }
 
 const ModalContainer = styled.div`
-  ${fixed({ y: '50%' })};
-  left: calc(50% - 100px);
-  width: 200px;
-  height: 100px;
-  display: ${props => (props.isOpen ? 'flex' : 'none')};
-  justify-content: center;
-  align-items: center;
-  padding: 1rem;
-  background-color: ${ThemeColors.black};
-  opacity: 0.9;
-  color: #eee;
-  border-radius: 5px;
-  z-index: 200;
+	${fixed({ y: '50%' })};
+	left: calc(50% - 100px);
+	width: 250px;
+
+	display: ${props => (props.isOpen ? 'flex' : 'none')};
+	justify-content: center;
+	align-items: center;
+	padding: 1rem;
+	background-color: ${ThemeColors.black};
+	opacity: 0.9;
+	color: #eee;
+	border-radius: 5px;
+	z-index: 200;
 `;
 
 const ModalHeader = styled.div`
-  text-align: center;
+	text-align: center;
 `;
